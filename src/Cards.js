c1346dd70c112bac0f9c06b8ebbe89beaa21ba6b
shivam khanna <shivam1854.cse18@chitkara.edu.in>
2021-05-24 18:13:30 +0530

    amravati-express update-1

--- src/Cards.js
+++ src/Cards.js
@@ -1,83 +1,43 @@
 import React from 'react'
-import {Card, Button} from 'react-bootstrap';
 import "./Cards.css";
+import data from "./data";
+
 const Cards = () => {
    
     return (
-        <div className="container">
-            <div className="row mt-5 ">
-                <div className="col-sm-3 col-3  my-auto "  >
-                <Card className="abc" border="light" style={{ width: '18rem', backgroundColor: "#F1F1F1"}}>
-                    <Card.Img variant="top" src="https://image.freepik.com/free-vector/digital-banking_23-2147517376.jpg" style={{padding: "20px"}}/>
-                    
-                    <Card.Body>
-                        <Card.Title style={{fontSize: "30px"}}>Bank</Card.Title>
-                        <Card.Title><Button  variant="outline-danger">Go somewhere</Button></Card.Title>
-                    </Card.Body>
-                </Card>
+        <div className="main row container">
+            {
+                data.map(({name, url}) =>{
+                    return(
+                        <div class="card col-4">
+                            <div class="image">
+                                <img src={url}/>
+                            </div>
+                            <div class="title">
+                                <h1>{name}</h1>
+                            </div>
+                            <div class="des">
+                                <p></p>
+                                <button>Show All</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            <div class="card col-4">
+                <div class="image">
+                    <img src="https://image.flaticon.com/icons/png/512/755/755195.png"/>
                 </div>
-
-                <div className="col-sm-3 col-3 my-auto">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div class="title">
+                    <h1>BARBER SHOP</h1>
                 </div>
-
-                <div className="col col-3 my-auto">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div class="des">
+                    <p></p>
+                    <button>Read More...</button>
                 </div>
-
-                <div className="col col-3 my-auto">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </div>
-
-                <div className="col col-3 pt-3  mx-auto">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </div>
-            </div>
-            <div>
-                
             </div>
         </div>
-    )
+    ) 
 }
 
 export default Cards
