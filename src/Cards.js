a19797beaa746a0cc143246fe94b4d349f1d97d3
shivam khanna <shivam1854.cse18@chitkara.edu.in>
2021-05-28 23:59:56 +0530

    amravati-express update-2

--- src/Cards.js
+++ src/Cards.js
@@ -1,42 +1,64 @@
 import React from 'react'
-import "./Cards.css";
+import { NavLink } from 'react-router-dom';
+// import "./Cards.css";
+import "./Card.css";
 import data from "./data";
 
 const Cards = () => {
    
     return (
-        <div className="main row container">
-            {
-                data.map(({name, url}) =>{
-                    return(
-                        <div class="card col-4">
-                            <div class="image">
-                                <img src={url}/>
-                            </div>
-                            <div class="title">
-                                <h1>{name}</h1>
-                            </div>
-                            <div class="des">
-                                <p></p>
-                                <button>Show All</button>
-                            </div>
+        // <div className="main row container">
+        //     {
+        //         data.map(({name, url}) =>{
+        //             return(
+        //                 <div class="card col-4">
+        //                     <div class="image">
+        //                         <img src={url} alt="shops"/>
+        //                     </div>
+        //                     <div class="title">
+        //                         <h1>{name}</h1>
+        //                     </div>
+        //                     <div class="des">
+        //                         <p></p>
+        //                         <button>Show All</button>
+        //                     </div>
+        //                 </div>
+        //             )
+        //         })
+        //     }
+        // </div>
+
+        <>
+            <div className="my-5">
+                <h1 className="text-center" style={{fontSize: "60px"}} >SHOPS</h1>
+            </div>
+            <div className="container mb-5" >
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-5">
+                            {
+                                data.map(({name, url}) =>{
+                                    return(
+                                        <div className="col-md-4 col-10 mx-auto" >
+                                            <div className="card" >
+                                                <img src={url} alt="img" />
+                                                <div className="card-body" >
+                                                    <h1 className="card-title font-weight-bold" >{name}</h1>
+                                                    <p className="card-text" ></p>
+                                                    <NavLink to="" className="btn btn-primary" >
+                                                        show all
+                                                    </NavLink>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
-                    )
-                })
-            }
-            <div class="card col-4">
-                <div class="image">
-                    <img src="https://image.flaticon.com/icons/png/512/755/755195.png"/>
-                </div>
-                <div class="title">
-                    <h1>BARBER SHOP</h1>
-                </div>
-                <div class="des">
-                    <p></p>
-                    <button>Read More...</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     ) 
 }
 
---
+++ src/CardsData.js
@@ -0,0 +1,14 @@
+const CardsData=[
+    {name: "Bank", url: "https://image.flaticon.com/icons/png/512/755/755195.png"},
+    {name: "Barber", url: "https://image.flaticon.com/icons/png/512/1995/1995573.png"},
+    {name: "Daily Essentials", url: "https://image.flaticon.com/icons/png/512/273/273177.png"},
+    {name: "Dentist", url: "https://image.flaticon.com/icons/png/512/893/893862.png"},
+    {name: "Fast Food", url: "https://image.flaticon.com/icons/png/512/1037/1037762.png"},
+    {name: "Fine Dine", url: "https://image.flaticon.com/icons/png/512/3003/3003998.png"},
+    {name: "Groceries", url: "https://image.flaticon.com/icons/png/512/1261/1261163.png"},
+    {name: "Ice Cream", url: "https://image.flaticon.com/icons/png/512/3157/3157358.png"},
+    {name: "Pharmacy", url: "https://image.flaticon.com/icons/png/512/3047/3047613.png"}
+]
+
+export default CardsData;
+
