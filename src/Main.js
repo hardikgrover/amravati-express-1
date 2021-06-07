69ddc09a04f0d21a9842a2a38a74d6aa797d1463
Hardik Grover <hardikgrover@pop-os.localdomain>
2021-06-08 00:00:04 +0530

    fjldjf

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import StateWise from './StateWise';
-import Cards from './Cards';
-import Carousels from './Carousels';
+import React from "react";
+import StateWise from "./StateWise";
+import Cards from "./Cards";
+import Carousels from "./Carousels";
 
 function Main() {
-    return (
-         <div>
-             <Carousels/>
+  return (
+    <div>
+      {/* <Carousels/>
              {/* main div including cards */}
-             <Cards/>
-
-             {/* india api */}
-             {/* <div className="text-center">
+      {/* <Cards /> */}
+      {/* india api */}
+      {/* <div className="text-center">
                  <h1 className="abc">INDIA COVID DATA</h1>
              </div> */}
-             <StateWise/>
-         </div>
-    )
+      {/* <StateWise /> */} */}
+    </div>
+  );
 }
 
-export default Main
+export default Main;
